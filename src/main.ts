c9d09bf5fb4f2253051b69247f6d66056c09a46b
JuanMancuSidetool <juan.mancuso@sidetool.co>
2025-02-13 14:53:18 -0300

    enable new url

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
-// nest-backend/src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: ['http://localhost:3001', 'https://star-wars-front-eta.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
-
   await app.listen(3000);
 }
 
