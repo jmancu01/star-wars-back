cd45ae4122e10f73a6e835c9b5576de861e6a79c
JuanMancuSidetool <juan.mancuso@sidetool.co>
2025-02-13 15:50:59 -0300

    add .env variables

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3001', 'https://star-wars-front-eta.vercel.app'],
+    origin: [process.env.CORS_ORIGIN],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
 
-  // Use the PORT environment variable or fallback to 8000
   const port = process.env.PORT || 8000;
   await app.listen(port);
   console.log(`Application is running on port ${port}`);
